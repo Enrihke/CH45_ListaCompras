0589d07242ef8330062c282c0c4796a46a29d5f7
Enrique Gutierrez <khike.gtz@gmail.com>
2024-09-02 17:30:51 -0600

    Se agregan archivos

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const btnAgregar = document.getElementById('btnAgregar');
+const txtNombre = document.getElementById('Name');
+const txtNumber = document.getElementById('Number');
+const alertValidaciones = document.getElementById('alertValidaciones');
+const alertValidacionesTexto = document.getElementById('alertValidacionesTexto');
+
+function validarCantidad(){
+    if (txtNumber.value.lenght==0){
+        return false;
+    }//length==0
+
+    if (isNaN(txtNumber.value)){
+        return false;
+    } //isNaN()
+
+    if(Number(txtNumber.value)<=0){
+        return false;
+    }//<=0
+
+    return true;
+
+}//validarCantidad()
+
+btnAgregar.addEventListener("click", function (event){
+    event.preventDefault();
+        txtNombre.style.border="";
+        txtNumber.style.border="";
+        alertValidacionesTexto.innerHTML="";
+        alertValidaciones.style.display="none";
+// Vaidar el nombre del producto
+    if(txtNombre.value.lenght<3){
+        txtNombre.style.border="solid red medium";
+    alertValidacionesTexto.innerHTML="El <strong>Nombre</strong> no es correcto. <br/>"
+        alertValidaciones.style.display="block"
+        return false;
+    }//if lenght<3
+
+ //validar la cantidad
+    if (! validarCantidad()){
+        txtNumber.style.border="solid red medium";
+        alertValidacionesTexto.innerHTML+="La <strong>Cantidad</strong> no es correcta. <br/>";
+        alertValidaciones.style.display="block";
+    }//! validarcantidad
+
+}); //btnAgregar.addEventListener
+
+// evento blur es cuando un campo pierde el foco, se sale del campo
+txtNumber.addEventListener("blur", function(event){
+    txtNombre.value = txtNombre.value.trim();
+}); // txtNombre.addEventListener
+
+txtNumber.addEventListener("blur", function(event){
+    txtNumber.value = txtNumber.value.trim();
+}); // txtNumber.addEventListener
+
